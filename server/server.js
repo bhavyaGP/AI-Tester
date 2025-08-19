f228087ae77843c3b221a4cfefe24d60cc31d40b
Bhavya <bgprajapati575@gmail.com>
2025-08-19 12:18:35 +0530

    adding CRUD

--- server/server.js
+++ server/server.js
@@ -4,26 +4,28 @@ const db = []
 
 
 app.get('/getdata', (req, res) => {
-    res.status(200).json(db);
+    res.json(db);
 })
 app.post('/postdata', (req, res) => {
     const newData = req.body;
     db.push(newData);
-    res.status(201).json(newData);
+    res.json(newData);
 })
 
 app.delete('/deletedata/:id', (req, res) => {
     const { id } = req.params;
     db = db.filter(item => item.id !== id);
-    res.status(204).send();
+    res.status(204).json();
 })
 app.put('/updatedata/:id', (req, res) => {
     const { id } = req.params;
     const updatedData = req.body;
     db = db.map(item => item.id === id ? updatedData : item);
-    res.status(200).json(updatedData);
+    res.json(updatedData);
 })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+module.exports = app;
